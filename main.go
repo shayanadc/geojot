3f8e6bec07e19e1039f02661ece99654970185f9
Shayan Hosseini <shho@boozt.com>
2024-06-28 11:25:57 +0200

    added first hello world route

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "hello world"})
+
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
